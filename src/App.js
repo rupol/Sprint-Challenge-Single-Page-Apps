2bb72c8485bf207acbe09d354c10e582854ce6ea
Ruth Poliakon <r.poliakon@gmail.com>
2019-09-15 17:26:13 -0600

    character list added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 
+import LocationsList from "./components/LocationsList";
 
 export default function App() {
   return (
     <main>
       <Header />
       {/* <TabNav /> */}
+      {/* <CharacterList /> */}
+      <LocationsList />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import { Card } from "semantic-ui-react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+// STYLING
+const CardContainer = styled.div`
+  background: white;
+  opacity: 0.7;
+  margin: 30px 10px;
+  padding: 5px;
+  width: 300px;
+  border: 1px solid grey;
+  border-radius: 5px;
+`;
+
+const Image = styled.img`
+  max-width: 290px;
+`;
+
+const Name = styled.h2`
+  font-size: 1.8rem;
+`;
+
+const Info = styled.h3`
+  font-size: 1.2rem;
+`;
+
+export default function CharacterCard(props) {
+  return (
+    <CardContainer>
+      <Image src={props.image} alt={props.name} />
+      <Name>{props.name}</Name>
+      <h3>Status: {props.status}</h3>
+      <h3>Species: {props.species}</h3>
+    </CardContainer>
+  );
 }
